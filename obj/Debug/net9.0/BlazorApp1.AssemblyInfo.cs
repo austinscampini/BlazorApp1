f00288727853e2d2e4f373054c66430668e875d5
austinscampini <austinscampini@gmail.com>
2025-09-09 20:49:34 -0400

    Push amplify.yml file

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49580ff1f52184c3744d3d9ed346f7b26b2cdc35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+193c375bc564a09b95cd26b7e4f3dafbea1b645c")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
