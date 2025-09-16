d9f50c6d3f8f75f0e9c1615a3f296b02b1f80242
austinscampini <austinscampini@gmail.com>
2025-09-15 20:00:54 -0400

    Lowered the page delay and misc

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dcc1e7c84a4dcfe601a75b02152bc40a392aea00")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cba7c25d49ecc7eb9c1805cf15c59c2e68fbce47")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
