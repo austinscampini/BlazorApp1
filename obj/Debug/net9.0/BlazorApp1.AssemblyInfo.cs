b0c10c9ea92e70961e92e60e84af13f1cad8ae0a
austinscampini <austinscampini@gmail.com>
2025-09-25 18:32:27 -0400

    Small Updates
    Small misc updates

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6296f483e9bf48252e1decd66279501e792a36d8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+faeb1f247091d276c2f6208f7718ff1009abb874")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
