84cad9f7472125452194a0c4a4a066e61d8b86d4
austinscampini <austinscampini@gmail.com>
2025-11-02 13:40:19 -0500

    Page Updates
    Added more navigation between pages, tried to make things more fluent, also added some updates to user profile

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9375236e48e20dd9c0d823671712c41ad0ff2a85")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f09dfee199d5b49e403838cde8094f45b39ba08a")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
