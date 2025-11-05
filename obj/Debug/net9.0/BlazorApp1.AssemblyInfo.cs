da45895327d9031fe4bfba8fce6e790dfb2587b5
austinscampini <austinscampini@gmail.com>
2025-11-04 19:04:29 -0500

    Updating playlist full view
    Making it more mobile friendly

--- obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net9.0/BlazorApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f09dfee199d5b49e403838cde8094f45b39ba08a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84cad9f7472125452194a0c4a4a066e61d8b86d4")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
